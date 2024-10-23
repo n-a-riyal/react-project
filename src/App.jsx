8d71715702d58e805386db6110b2264004b74cbb
Nure Alam Riyal <20223656640alam@juniv.edu>
2024-10-23 21:57:12 +0600

    project set up and header add

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+
+import './App.css'
+import Header from './Header/Header';
+
+function App() {
+
+
+  return (
+    <>
+  <header className='w-11/12 mx-auto'><Header></Header></header>
+  <main className='w-11/12 mx-auto'>
+  <div className='flex'>
+    <div></div>
+    <div></div>
+  </div>
+
+  </main>
+    </>
+  )
+}
+
+export default App
